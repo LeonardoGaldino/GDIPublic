c2133e4f837077895906640aca9338be19e4d1ed
LeonardoGaldino <lcgm@cin.ufpe.br>
2017-09-02 23:27:44 -0300

    Refactor on gulpfile production tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 //gulp imported
 var gulp = require('gulp');
+var gulpsync = require('gulp-sync')(gulp);
 
 //gulp plugins imported
 var concat = require('gulp-concat');
@@ -7,18 +8,22 @@ var cssmin = require('gulp-minify-css');
 var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
 
-var js_globs = ['./node_modules/jquery/dist/jquery.js',
+var dev_js_globs = ['./node_modules/jquery/dist/jquery.js',
                 './node_modules/materialize-css/dist/js/materialize.js',
                 './js/*.js'
                 ];
 
-var css_globs = ['./css/*.css',
+var dev_css_globs = ['./css/*.css',
                  './node_modules/materialize-css/dist/css/materialize.css'
                  ];
 
+var prod_js_path = './dist/js/app.min.js';
+
+var prod_css_path = './dist/css/main.min.css';
+
 
 gulp.task('dev_scripts', function() {
-  return gulp.src(js_globs)
+  return gulp.src(dev_js_globs)
     .pipe(concat('app.js'))
     .pipe(gulp.dest('./dist/js/'))
     .pipe(rename({
@@ -28,7 +33,7 @@ gulp.task('dev_scripts', function() {
 });
 
 gulp.task('dev_styles', function() {
-  return gulp.src(css_globs)
+  return gulp.src(dev_css_globs)
     .pipe(concat('main.css'))
     .pipe(gulp.dest('./dist/css/'))
     .pipe(rename({
@@ -37,34 +42,25 @@ gulp.task('dev_styles', function() {
     .pipe(gulp.dest('./dist/css/'));
 });
 
-//tasks for preparing for production
+//tasks for minifying
 gulp.task('prod_scripts', function() {
-  return gulp.src(js_globs)
-    .pipe(concat('app.js'))
-    .pipe(gulp.dest('./dist/js/'))
+  return gulp.src(prod_js_path)
     .pipe(uglify())
-    .pipe(rename({
-      suffix: '.min'
-    }))
     .pipe(gulp.dest('./dist/js/'));
 });
 
 gulp.task('prod_styles', function() {
-  return gulp.src(css_globs)
-    .pipe(concat('main.css'))
-    .pipe(gulp.dest('./dist/css/'))
+  return gulp.src(prod_css_path)
     .pipe(cssmin())
-    .pipe(rename({
-      suffix: '.min'
-    }))
     .pipe(gulp.dest('./dist/css/'));
 });
 
 gulp.task('default', ['dev_scripts', 'dev_styles']);
 
-gulp.task('prod', ['prod_scripts', 'prod_styles']);
+gulp.task('prod', gulpsync.sync(['dev_scripts', 'prod_scripts',
+                                 'dev_styles', 'prod_styles']));
 
 gulp.task('watch', function() {
-  gulp.watch(js_globs, ['dev_scripts']);
-  gulp.watch(css_globs, ['dev_styles']);
+  gulp.watch(dev_js_globs, ['dev_scripts']);
+  gulp.watch(dev_css_globs, ['dev_styles']);
 });
