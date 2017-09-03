f99dc6a20d3bfcedde6179ce3b9e0109a19b2963
LeonardoGaldino <lcgm@cin.ufpe.br>
2017-09-03 19:31:05 -0300

    Refactors on gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -8,20 +8,26 @@ var cssmin = require('gulp-minify-css');
 var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
 
-var dev_js_globs = ['./node_modules/jquery/dist/jquery.js',
+//All js needed for the project
+var dev_js_globs = [
+                './node_modules/jquery/dist/jquery.js',
                 './node_modules/materialize-css/dist/js/materialize.js',
                 './js/*.js'
                 ];
 
-var dev_css_globs = ['./css/*.css',
-                 './node_modules/materialize-css/dist/css/materialize.css'
+//All css needed for the project
+var dev_css_globs = [
+                    './node_modules/materialize-css/dist/css/materialize.css',
+                    './css/*.css'
                  ];
 
+//Path for output JS file
 var prod_js_path = './dist/js/app.min.js';
 
+//Path for output CSS file
 var prod_css_path = './dist/css/main.min.css';
 
-
+//Development tasks
 gulp.task('dev_scripts', function() {
   return gulp.src(dev_js_globs)
     .pipe(concat('app.js'))
@@ -42,7 +48,7 @@ gulp.task('dev_styles', function() {
     .pipe(gulp.dest('./dist/css/'));
 });
 
-//tasks for minifying
+//Tasks for minifying (production)
 gulp.task('prod_scripts', function() {
   return gulp.src(prod_js_path)
     .pipe(uglify())
@@ -55,11 +61,13 @@ gulp.task('prod_styles', function() {
     .pipe(gulp.dest('./dist/css/'));
 });
 
+//Main tasks
 gulp.task('default', ['dev_scripts', 'dev_styles']);
 
 gulp.task('prod', gulpsync.sync(['dev_scripts', 'prod_scripts',
                                  'dev_styles', 'prod_styles']));
 
+//Watch for file change task
 gulp.task('watch', function() {
   gulp.watch(dev_js_globs, ['dev_scripts']);
   gulp.watch(dev_css_globs, ['dev_styles']);
