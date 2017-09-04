d171588565c79203bef794852697aa9e4626db29
LeonardoGaldino <lcgm@cin.ufpe.br>
2017-09-04 19:05:07 -0300

    Avoid loading page if user clicks on current page

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
+var currentLocation;
 //Function to load the content of the specified page into specified element
+
 function changeLocation(targetLocation){
-    $('#loading-circle').show();
-    $('#location-content').load('/pages/'+targetLocation+'.html', ()=>{
-        $('#loading-circle').hide();
-    });
+    if(currentLocation != targetLocation){
+        $('#loading-circle').show();
+        $('#location-content').load('/pages/'+targetLocation+'.html', ()=>{
+            $('#loading-circle').hide();
+        });
+        currentLocation = targetLocation;
+    }
 }
 
 $(document).ready(function(){
