0066bb55ff09adae05258abd5a61370157a195be
LeonardoViitra <leonardo@viitra.com>
2017-09-05 13:47:54 -0300

    Refactoring gulpfile and project structure

--- gulpfile.js
+++ gulpfile.js
@@ -12,53 +12,60 @@ var uglify = require('gulp-uglify');
 var dev_js_globs = [
                 './node_modules/jquery/dist/jquery.js',
                 './node_modules/materialize-css/dist/js/materialize.js',
-                './js/*.js'
+                './app/js/*.js'
                 ];
 
 //All css needed for the project
 var dev_css_globs = [
                     './node_modules/materialize-css/dist/css/materialize.css',
-                    './css/*.css'
+                    './app/css/*.css'
                  ];
 
+//Path for JS folder
+var prod_js_folder = './app/dist/js/';
+
 //Path for output JS file
-var prod_js_path = './dist/js/app.min.js';
+var prod_js_path = (prod_js_folder + 'app.min.js');
+
+//Path for CSS folder
+var prod_css_folder = './app/dist/css/';
 
 //Path for output CSS file
-var prod_css_path = './dist/css/main.min.css';
+var prod_css_path = (prod_css_folder + 'main.min.css');
+
 
 //Development tasks
 gulp.task('dev_scripts', function() {
   return gulp.src(dev_js_globs)
     .pipe(concat('app.js'))
-    .pipe(gulp.dest('./dist/js/'))
+    .pipe(gulp.dest(prod_js_folder))
     .pipe(rename({
       suffix: '.min'
     }))
-    .pipe(gulp.dest('./dist/js/'));
+    .pipe(gulp.dest(prod_js_folder));
 });
 
 gulp.task('dev_styles', function() {
   return gulp.src(dev_css_globs)
     .pipe(concat('main.css'))
-    .pipe(gulp.dest('./dist/css/'))
+    .pipe(gulp.dest(prod_css_folder))
     .pipe(rename({
       suffix: '.min'
     }))
-    .pipe(gulp.dest('./dist/css/'));
+    .pipe(gulp.dest(prod_css_folder));
 });
 
 //Tasks for minifying (production)
 gulp.task('prod_scripts', function() {
   return gulp.src(prod_js_path)
     .pipe(uglify())
-    .pipe(gulp.dest('./dist/js/'));
+    .pipe(gulp.dest(prod_js_folder));
 });
 
 gulp.task('prod_styles', function() {
   return gulp.src(prod_css_path)
     .pipe(cssmin())
-    .pipe(gulp.dest('./dist/css/'));
+    .pipe(gulp.dest(prod_css_folder));
 });
 
 //Main tasks
