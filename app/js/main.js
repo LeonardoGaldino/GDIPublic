061d5d9efae4d9e14f6e728a5ad71885c87eeca1
LeonardoGaldino <lcgm@cin.ufpe.br>
2017-09-05 21:44:25 -0300

    Making menu sidenav

--- app/js/main.js
+++ app/js/main.js
@@ -2,16 +2,19 @@ var currentLocation;
 //Function to load the content of the specified page into specified element
 
 function changeLocation(event){
-    var targetLocation = $(event).length > 0 ? $(event.target).attr('id') : event;
+
+    var targetLocation = ($(event).length > 0 ? $(event.target).attr('id') : event);
 
     if(currentLocation != targetLocation){
         $('#loading-circle').show();
-        $('#location-content').load('/app/pages/'+targetLocation+'.html', function(){
         $('#location-content').load('/pages/'+targetLocation+'.html', function(){
             $('#loading-circle').hide();
         });
         currentLocation = targetLocation;
+        if(window.innerWidth < 993)
+            $(".drag-target").click();
     }
+
 }
 
 
@@ -19,4 +22,5 @@ $(document)
 .on('click', 'a.openPage', changeLocation)
 .ready(function(){
     changeLocation('home');
+    $(".button-collapse").sideNav();
 });
