f7f1d666854295cf1bc6d7380b047602b9931a75
LeonardoGaldino <lcgm@cin.ufpe.br>
2017-09-06 23:29:55 -0300

    Adding current page location to sidenav

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,17 @@
 var currentLocation;
 //Function to load the content of the specified page into specified element
 
-function changeLocation(event){
+function updateBackgroundColor(curPage, nextPage) {
+    $('#'+curPage).removeClass('selected-page');
+    $('#'+nextPage).addClass('selected-page');
+}
+
+function changeLocation(event) {
 
     var targetLocation = ($(event).length > 0 ? $(event.target).attr('id') : event);
 
     if(currentLocation != targetLocation){
+        updateBackgroundColor(currentLocation, targetLocation);
         $('#main-card').hide();
         $('#loading-circle').show();
         $('#location-content').load('/pages/'+targetLocation+'.html', function(){
