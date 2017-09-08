680d46ef45955a530e873510b90cbf344533256f
LeonardoGaldino <lcgm@cin.ufpe.br>
2017-09-07 21:43:44 -0300

    Refactors on main js file

--- app/js/main.js
+++ app/js/main.js
@@ -5,7 +5,7 @@ function updateBackgroundColor(curPage, nextPage) {
     $('#'+nextPage).addClass('selected-page');
 }
 
-function addPageClasses(nextPage) {
+function removeOrAddMainCardClass(nextPage){
     var element = $('#main-card-panel');
     if(nextPage == 'disciplineContent'){
         element.addClass('main-card-panel-responsive');
@@ -15,17 +15,24 @@ function addPageClasses(nextPage) {
     }
 }
 
+function addPageClasses(nextPage) {
+    removeOrAddMainCardClass(nextPage);
+    if(nextPage == 'pdfs'){
+        loadPDFPage();
+    }
+}
+
 //Function to load the content of the specified page into specified element
 function changeLocation(event) {
     //Get ID of the tab clicked (event parameter can be the click event or the ID itself)
     var targetLocation = ($(event).length > 0 ? $(event.target).attr('id') : event);
 
     if(currentLocation != targetLocation){
-        addPageClasses(targetLocation);
         updateBackgroundColor(currentLocation, targetLocation);
         $('#main-card').hide();
         $('#loading-circle').show();
         $('#location-content').load('/pages/'+targetLocation+'.html', function(){
+            addPageClasses(targetLocation);
             $('.modal').modal();
             $('#loading-circle').hide();
             $('#main-card').show();
